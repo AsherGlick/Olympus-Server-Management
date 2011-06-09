21ecb8fe630ffa163d30b13f6781567be20d994c
Asher Glick <asher.glick@gmail.com>
2011-06-09 16:20:37 -0400

    added licence info

--- Node/client.py
+++ Node/client.py
@@ -1,3 +1,22 @@
+# This code is part of the Olympus Server Management Program
+#Copyright (C) 2011  Asher Glick
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+
 # Echo client program
 import socket
 import sys
@@ -7,12 +26,15 @@ import time
 
 # load plugins
 print '\n--- plugin list ---'
+fileList = os.listdir('./plugins');
 for f in os.listdir('./plugins'):
   if (f is " PLUGINS "):
     print "hello world"
-  print '|', f, '|'
+  print f
+ 
 print '--- end plugin list ---\n'
-    
+
+print "loaded", len(fileList), "plugins"
 
 # connect to remote computer
 HOST = '127.0.0.1'    # The remote host
--- Root/Backend/main.cpp
+++ Root/Backend/main.cpp
@@ -1,7 +1,33 @@
+/*
+This code is part of the Olympus Server Management Program
+Copyright (C) 2011  Asher Glick
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <iostream>
+#include <list>
 #include <pthread.h>
 using namespace std;
 
-int main() {
-  
+struct socket
+{
+};
+
+int main()
+{
+  // create two extra threads. one uses the 'wait for new socket' function
+  // the other uses the 'check known sockets'
 }
