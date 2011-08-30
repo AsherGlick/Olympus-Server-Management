48b26076ebd8767c8077c70b104f49b97ccfc129
Asher Glick <asher.glick@gmail.com>
2011-08-30 14:33:46 -0400

    ${2}

---
+++ DevTools/PluginCreator/generalStats.py
@@ -0,0 +1,13 @@
+import time
+import os
+sendOut("hello world, i am a plugin","seven")
+
+# Linux HDD Size / Used
+s = os.statvfs('/')
+bytesAvailable = (s.f_bavail * s.f_frsize) / 1024;
+bytesTotal = (s.f_blocks * s.f_frsize) / 1024
+bytesUsed = bytesTotal - bytesAvailable;
+percentFree = (bytesAvailable*100)/(bytesTotal)
+percentUsed = 100-percentFree
+sendOut("bytesFree",str(bytesAvailable),"\nbytesTotal",str(bytesTotal),"\npercentUsed",str(percentUsed),"\npercentFree",str(percentFree))
+time.sleep(220)
