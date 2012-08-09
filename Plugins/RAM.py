4aa11b8cf2381b88eb1f383e1db41a493d3a3ae2
unknown <Jim@Winterfell.(none)>
2012-08-09 19:53:15 -0400

    Commented color UI, created data save file

--- Plugins/RAM.py
+++ Plugins/RAM.py
@@ -46,22 +46,26 @@ if system ==  "Windows":#if windows...
 	
 	
 	winmem()	
+	myfile = file("RAMTEST.txt",'x')
+else break
+
 	
 
 # The following runs for Linux (unix based) operating Systems	
-if platform.name() == 'Linux':
+#if platform.name() == 'Linux':
 
-	class MemoryMonitor(object):
+#	class MemoryMonitor(object):
 
-		def __init__(self, username):
-			"""Create new MemoryMonitor instance."""
-			self.username = username
+#		def __init__(self, username):
+#			"""Create new MemoryMonitor instance."""
+#			self.username = username
 
-		def usage(self):
-			"""Return int containing memory used by user's processes."""
-			self.process = subprocess.Popen("ps -u %s -o rss | awk '{sum+=$1} END {print sum}'" % self.username,
-				shell=True,
-				stdout=subprocess.PIPE,
-				)
-			self.stdout_list = self.process.communicate()[0].split('\n')
-			return int(self.stdout_list[0])
+#		def usage(self):
+#			"""Return int containing memory used by user's processes."""
+#			self.process = subprocess.Popen("ps -u %s -o rss | awk '{sum+=$1} END {print sum}'" % self.username,
+#				shell=True,
+#				stdout=subprocess.PIPE,
+#				)
+#			self.stdout_list = self.process.communicate()[0].split('\n')
+#			return int(self.stdout_list[0])
+#
